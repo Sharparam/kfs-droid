3cd821857da0b114fcc323e5d713e06c993ce551
Adam Hellberg <sharparam@sharparam.com>
2017-04-12 13:12:01 +0200

    Clean up MovieActivity

--- app/src/main/java/com/sharparam/kfs/MovieActivity.kt
+++ app/src/main/java/com/sharparam/kfs/MovieActivity.kt
@@ -2,15 +2,12 @@ package com.sharparam.kfs
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import android.widget.Toast
 import com.sharparam.kfs.api.Movie
 import kotlinx.android.synthetic.main.activity_movie.*
 import kotlinx.android.synthetic.main.content_movie.*
-import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.uiThread
+import org.jetbrains.anko.*
 
-class MovieActivity : AppCompatActivity() {
+class MovieActivity : AppCompatActivity(), AnkoLogger {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie)
@@ -18,11 +15,10 @@ class MovieActivity : AppCompatActivity() {
 
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
-        val intent = intent
         val id = intent.getIntExtra("id", -1)
 
         if (id == -1) {
-            Toast.makeText(this, "Invalid movie", Toast.LENGTH_SHORT).show()
+            toast("Invalid movie")
             return
         }
 
@@ -40,19 +36,15 @@ class MovieActivity : AppCompatActivity() {
                 description.loadMarkdown(movie.description)
 
                 doAsync {
-                    Log.d(TAG, "doInBackground: Obtaining bitmap")
+                    debug("doInBackground: Obtaining bitmap")
                     val bitmap = movie.posterBitmap
 
                     uiThread {
-                        Log.d(TAG, "onPostExecute: Bitmap obtained, setting imageview")
+                        debug("onPostExecute: Bitmap obtained, setting imageview")
                         poster.setImageBitmap(bitmap)
                     }
                 }
             }
         }
-    }
-
-    companion object {
-        private val TAG = MovieActivity::class.java.simpleName
     }
 }
