77098cd4766ece1889034f1850514967ed9c9178
Adam Hellberg <sharparam@sharparam.com>
2017-04-12 08:55:59 +0200

    Clean up code

--- app/src/main/java/com/sharparam/kfs/MainActivity.java
+++ app/src/main/java/com/sharparam/kfs/MainActivity.java
@@ -2,6 +2,7 @@ package com.sharparam.kfs;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
@@ -16,8 +17,6 @@ import com.sharparam.kfs.api.Movie;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, MoviesFragment.OnListFragmentInteractionListener {
-    private static final String TAG = MainActivity.class.getSimpleName();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,7 +47,7 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
 
         switch (item.getItemId()) {
