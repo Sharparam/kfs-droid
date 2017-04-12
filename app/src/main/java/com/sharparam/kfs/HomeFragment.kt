4fd900287a28ee8c5103e66823d1d669a255df6b
Adam Hellberg <sharparam@sharparam.com>
2017-04-12 15:26:21 +0200

    More cleanup

--- app/src/main/java/com/sharparam/kfs/HomeFragment.kt
+++ app/src/main/java/com/sharparam/kfs/HomeFragment.kt
@@ -1,34 +1,25 @@
 package com.sharparam.kfs
 
-import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.sharparam.kfs.api.Page
+import kotlinx.android.synthetic.main.fragment_home.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.support.v4.onUiThread
-import org.jetbrains.anko.support.v4.uiThread
-
 import org.json.JSONException
-
 import java.io.IOException
-
-import us.feras.mdv.MarkdownView
 
 class HomeFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater!!.inflate(R.layout.fragment_home, container, false)
-
-        val md = view.findViewById(R.id.md_home) as MarkdownView
 
         doAsync {
             val page = try {
@@ -40,7 +31,7 @@ class HomeFragment : Fragment() {
             }
 
             onUiThread {
-                md.loadMarkdown(page.content)
+                md_home.loadMarkdown(page.content)
             }
         }
 
